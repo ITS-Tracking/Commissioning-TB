eaf7134edf5089f4efe26cee42681796373cda96
Maximiliano Puccio <maximiliano.puccio@cern.ch>
2021-10-31 11:35:26 +0100

    Add pv finder to the commands

--- processing-scripts/command.sh
+++ processing-scripts/command.sh
@@ -1,4 +1,5 @@
-ConfPar='ITSVertexerParam.phiCut=0.5;ITSVertexerParam.clusterContributorsCut=3;ITSVertexerParam.tanLambdaCut=0.2;ITSCATrackerParam.useDiamond=true; ITSCATrackerParam.diamondPos[2]=35.; ITSCATrackerParam.pvRes=10.'
+ConfPar='ITSVertexerParam.phiCut=0.5;ITSVertexerParam.clusterContributorsCut=3;ITSVertexerParam.tanLambdaCut=0.2;'
+# ITSCATrackerParam.useDiamond=true; ITSCATrackerParam.diamondPos[2]=35.; ITSCATrackerParam.pvRes=10.'
 if [ ! -f done ]; then
 	if [ ! -f o2clus_its_.root ]; then
 		o2-ctf-reader-workflow --onlyDet ITS  --delay 0.01 --ctf-input data.lst --shm-segment-size 24000000000  --max-cached-files 10  | o2-its-reco-workflow --clusters-from-upstream --trackerCA --tracking-mode sync_misaligned --disable-mc --configKeyValues "$ConfPar"  --autosave 10 --run -b && mv o2clus_its.root o2clus_its_.root
@@ -7,5 +8,12 @@ if [ ! -f done ]; then
 	fi
 	touch done					
 else
-	echo "All done here..."
+	echo "Clusters and tracks reconstruction done here."
+fi
+
+if [ ! -f pv_done ]; then
+	o2-primary-vertexing-workflow --vertex-track-matching-sources ITS --vertexing-sources ITS -b --disable-mc --hbfutils-config none --configKeyValues "HBFUtils.orbitFirstSampled=295044346" --run
+	touch pv_done
+else
+	echo "Primary vertex reconstructed"
 fi
