5e443a6f26f243846a31d9f648e29d1491fa913e
fmazzasc <fmazzasc@cern.ch>
2021-12-10 11:31:34 +0100

    Fix MC embedding

--- MC/run_embedding.sh
+++ MC/run_embedding.sh
@@ -1,25 +1,27 @@
-#!/usr/bin/env bash
 
 set -x
 
-MODULES="PIPE ITS TPC TRD TOF FT0 FV0"
-BKGEVENTS=1000
+BKGEVENTS=10
 SIGEVENTS=1000
+INTERACTIONRATE=500000
 NWORKERS=$(grep 'cpu[0-9]' /proc/stat | wc -l)
 
 export O2DPG_ROOT=/home/fmazzasc/alice/O2DPG
 
-# generate background
+# #generate background
 
-o2-sim -j ${NWORKERS} -n ${BKGEVENTS} -g pythia8pp -m ${MODULES} -o bkg \
+o2-sim -j ${NWORKERS} -n ${BKGEVENTS} -g pythia8pp --skipModules MCH,MFT,ZDC -o bkg \
        --configFile $O2DPG_ROOT/MC/config/common/ini/basic.ini \
        | tee logbkg 2>&1    
 
 # embed signal into background
 
-o2-sim -j ${NWORKERS} -n ${SIGEVENTS} -g boxgen -m ${MODULES} -o sgn \
+o2-sim -j ${NWORKERS} -n ${SIGEVENTS} -g boxgen --skipModules MCH,MFT,ZDC -o sgn \
        --configKeyValues 'BoxGun.pdg=1010010030;BoxGun.eta[0]=-0.9;BoxGun.eta[1]=0.9;BoxGun.prange[0]=0.5;BoxGun.prange[1]=10.' \
        --embedIntoFile bkg_Kine.root \
-       | tee logsgn 2>&1
-o2-sim-digitizer-workflow -b --sims bkg,sgn
+       # | tee logsgn 2>&1
+
+
+o2-steer-colcontexttool -i bkg,${INTERACTIONRATE:-400000},${SIGEVENTS}:${BKGEVENTS} sgn,@0:e1 --show-context
+o2-sim-digitizer-workflow -b --sims bkg,sgn --incontext collisioncontext.root
 o2-trd-trap-sim -b
